2a66696cc960d318039007920a9c5584d2fff085
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-02-24 20:28:16 +0500

    Done

---
+++ node.js
@@ -0,0 +1,20 @@
+// let fs = require("fs");
+
+// readFile("./src/assets/images/grid-images/1.png", "utf-8", (err, data) => {
+//   console.log(data);
+// });
+
+// const dir = "./src/assets/images/grid-images/";
+// const files = fs.readdirSync(dir);
+
+// const file of files
+// for (let i = 0; i < files.length; i++) {
+//   fs.writeFile(
+//     "./data.js",
+//     `{img : gridImg${files[i]},isHoverable : false},`,
+//     { encoding: "utf-8", flag: "a" },
+//     (err) => {
+//       console.log(err);
+//     }
+//   );
+// }
